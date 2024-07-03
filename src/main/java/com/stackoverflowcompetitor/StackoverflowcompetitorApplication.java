ea266e642e29c4ddd57958d51db5fec4ecae559d
Shubham Sharma <s.psharma887@gmail.com>
2024-07-03 23:02:59 +0530

    Let's get started

---
+++ src/main/java/com/stackoverflowcompetitor/StackoverflowcompetitorApplication.java
@@ -0,0 +1,13 @@
+package com.stackoverflowcompetitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowcompetitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowcompetitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackoverflowcompetitor/StackoverflowcompetitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackoverflowcompetitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowcompetitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
